66f3c9f76533a6260fb7c9314cdff402dad48436
tcmpolly <bezuglova3011@mail.ru>
2026-01-18 04:53:38 +0300

    .

--- task5_2.py
+++ task5_2.py
@@ -1,32 +1,20 @@
-# Функция для вывода чисел от start до end
-def print_numbers(start, end):
-    """Выводит все числа от start до end включительно."""
-    print(f"Числа от {start} до {end}:")
+def decimal_to_binary(decimal_num):
+    """Переводит десятичное число в двоичное."""
+    if decimal_num == 0:
+        return "0"
     
-    if start < end:
-        # Возрастающий порядок
-        for number in range(start, end + 1):
-            print(number, end=" ")
-    else:
-        # Убывающий порядок
-        for number in range(start, end - 1, -1):
-            print(number, end=" ")
-    print()  # Переход на новую строку
+    binary = ""  # строка для хранения двоичного представления
+    
+    while decimal_num > 0:
+        remainder = decimal_num % 2  # остаток от деления на 2
+        binary = str(remainder) + binary  # добавляем остаток слева
+        decimal_num //= 2  # целочисленное деление на 2
+    
+    return binary
 
-# Основная программа
-print("=" * 40)
-print("ВЫВОД ЧИСЕЛ В ДИАПАЗОНЕ (с функцией)")
-print("=" * 40)
 
-# Получаем данные от пользователя
-A = int(input("Введите начальное число (A): "))
-B = int(input("Введите конечное число (B): "))
+# Запрашиваем число у пользователя
+num = int(input("Введите десятичное число: "))
+binary_result = decimal_to_binary(num)
 
-# Вызываем функцию
-print_numbers(A, B)
-
-# Дополнительные вызовы для демонстрации
-print("\nДополнительные примеры:")
-print_numbers(1, 5)     # 1 2 3 4 5
-print_numbers(10, 5)    # 10 9 8 7 6 5
-print_numbers(-3, 3)    # -3 -2 -1 0 1 2 3
+print(f"{num} в двоичной системе: {binary_result}")
---
+++ task6_1.py
@@ -0,0 +1,6 @@
+numbers = [14, 7, 6, 99, 45]
+user_num = int(input("Введите число: "))
+if user_num in numbers:
+    print("Поздравляю, Вы угадали число!")
+else:
+    print("Нет такого числа!")
